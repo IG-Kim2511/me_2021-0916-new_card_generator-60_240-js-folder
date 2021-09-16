0cbec77d76a8a03bdc2c95f6839490e2d9a3ff5f
iikim <iikim2511@gmail.com>
2021-09-16 14:52:18 -0400

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+
+    //   // 🍀    
+    //   class Card {
+    //     constructor(num, color){
+        
+
+    //         this.num = num;
+    //         this.color = color;
+
+    //         const mainElem = document.createElement('div');
+    //         mainElem.innerHTML = `
+    //         <div class="world">
+    //             <div class="a card-side card-side-front">F</div>
+    //             <div class="a card-side card-side-back">B</div>
+    //         </div>
+    //         `;
+    //         document.body.appendChild(mainElem);
+            
+    //     }
+    // }
+
+    // // 
+
+    // const card1 = new Card(1, 'green');
+    // const card2 = new Card(7, 'blue');
+
+    // // btnC.addEventListener('click',()=>{  
+    // // });
